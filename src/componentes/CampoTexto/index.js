2b5ea42fe5730022144c13bb1f577ec38137fe8e
Ana Judith <ana-judth@hotmail.com>
2023-02-08 16:48:23 -0300

    Criando componente card

---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,11 @@
+import './CampoTexto.css'
+const CampoTexto = ({label, placeholder}) => {
+    return (
+        <div className="campo-texto">
+            <label>{label}</label>
+            <input placeholder={placeholder}/>
+        </div>
+    )
+}
+
+export default CampoTexto
