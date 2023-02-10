e0d210bbae95bb89421e93a454faea22e1cc9b58
Ana Judith <ana-judth@hotmail.com>
2023-02-10 11:50:40 -0300

    criando lista suspensa

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,14 +1,26 @@
 import CampoTexto from "../CampoTexto";
+import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css";
 
 const Formulario = () => {
+
+  const times = [
+    'Programação',
+    'Front-End',
+    'Data Science',
+    'Devops',
+    'Ux e Design',
+    'Mobile',
+    'Inovação e Gestão'
+  ]
   return (
     <section className="formulario">
     <form>
         <h2>Preencha os dados para criar os card do colaborador</h2>
       <CampoTexto label="Nome" placeholder="Digite seu nome" />
       <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
-      <CampoTexto label="Imagem" placeholder="Anexe sua url" />
+      <CampoTexto label="Imagem" placeholder="Digite o endereço da url" />
+      <ListaSuspensa label="Time" itens={times}/>
     </form>
   </section>
   )
