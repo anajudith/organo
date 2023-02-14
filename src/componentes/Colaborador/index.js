57796c1d40a62b3ec74e136507e1ce97610dec35
Ana Judith <ana-judth@hotmail.com>
2023-02-13 22:25:35 -0300

    Criando card de colaborador

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,19 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <div>
+                    <img src='https://github.com/anajudith.png' alt='Ana Judith'/>
+                </div>
+                <div className='rodape'>
+                    <h4>Ana</h4>
+                    <h5>Front-end</h5>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador; 
